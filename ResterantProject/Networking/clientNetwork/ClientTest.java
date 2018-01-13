fa4047d816db23ec007830fe718529d2802dcac0
Ben-Laptop <Benjamin.Wass.2016@live.rhul.ac.uk>
2018-01-13 22:41:21 +0000

    Added system messages for client

--- ResterantProject/Networking/clientNetwork/ClientTest.java
+++ ResterantProject/Networking/clientNetwork/ClientTest.java
@@ -7,10 +7,13 @@ public class ClientTest {
 
     public static void main(String[] args) {
         Message request = new TestMessage(21);
+        System.out.println("Created message");
         ServerMessenger messenger = new ServerMessenger();
+        System.out.println("Created Messenger");
         try {
             messenger.sendMessage(request);
-            System.out.println(messenger.getReply().toString());
+            System.out.println("Message sent");
+            System.out.println("Message received from server: " + messenger.getReply().toString());
         } catch (ReplyNotReadException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
