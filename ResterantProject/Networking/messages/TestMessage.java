72a8f4b5758f24b2b930841699113331a2e90f97
Ben-Laptop <Benjamin.Wass.2016@live.rhul.ac.uk>
2018-01-14 17:22:37 +0000

    Added more JavaDoc

--- ResterantProject/Networking/messages/TestMessage.java
+++ ResterantProject/Networking/messages/TestMessage.java
@@ -1,22 +1,23 @@
 package messages;
 
-public class TestMessage implements Message{
+public class TestMessage implements Message {
 
     /**
-     * 
+     * TestMessage is used to test the connection to the sever. The reply to this
+     * should be a TestMessage from the server containing a string.
      */
     private static final long serialVersionUID = 1L;
     private String msg;
-    
+
     public TestMessage(String msg) {
         this.msg = msg;
     }
-    
+
     @Override
     public MessageType getMessageType() {
         return MessageType.TESTMSG;
     }
-    
+
     public String toString() {
         return "Test message containg the following test string: " + msg;
     }
