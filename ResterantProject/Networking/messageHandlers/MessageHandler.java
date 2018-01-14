588f79b170ed4fe002b5d26d76c5fb4251269b63
Ben-Laptop <Benjamin.Wass.2016@live.rhul.ac.uk>
2018-01-14 16:56:10 +0000

    Added Java Doc to MessageHandler

--- ResterantProject/Networking/messageHandlers/MessageHandler.java
+++ ResterantProject/Networking/messageHandlers/MessageHandler.java
@@ -4,5 +4,16 @@ import messages.Message;
 
 public interface MessageHandler {
 
+    /**
+     * This method is called by default so any action the handler will make will
+     * start in this method. As a result when this method is returned this object
+     * will be destroyed. In order for the correct handler to be called for the
+     * right message from the client MessageHandlerFinder will have to be updated.
+     * 
+     * @param msg
+     *            The message received from the client
+     * @return Returns the response the server wants to give. e.g. The designated
+     *         table number or a request for further information.
+     */
     public Message proccess(Message msg);
 }
